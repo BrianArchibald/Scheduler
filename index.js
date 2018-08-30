eb79c2d0fcbc28f3cd20622d51e193fd9b535e74
BrianArchibald <brianallenarchibald@gmail.com>
2018-08-30 18:32:19 -0500

    added js and css for main page

---
+++ index.js
@@ -0,0 +1,18 @@
+const nav = document.querySelector('.nav-container-main');
+const mobileNavIcon = document.querySelector('.moblie-nav-icon');
+const mobileNavList = document.querySelector('#mobile-nav');
+const closeMenu = document.querySelector('#close-menu');
+
+function changeMobileNav() {
+  if (nav.style.display != "none") {
+    mobileNavIcon.style.display = "none";
+    nav.style.display = "none";
+    mobileNavList.style.display = "block";
+  } else {
+    mobileNavIcon.style.display = "inline-block";
+    nav.style.display = "grid";
+    mobileNavList.style.display = "none";
+  }
+}
+mobileNavIcon.addEventListener('click', changeMobileNav);
+closeMenu.addEventListener('click', changeMobileNav);
